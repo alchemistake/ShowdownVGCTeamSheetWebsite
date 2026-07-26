50c85fea3c578594d627409eff0940954995d06e
Kasper Wissendorf <kw2011@live.dk>
2026-07-26 12:48:34 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Showdown to VGC Team Sheet</h1>
+      <p>Convert your Showdown team to a VGC team sheet!</p>
+    </div>
+  )
+}
